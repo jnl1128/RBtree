efb0037226dd48b2651de48b4c76001b3061f0f0
Ubuntu <ubuntu@ip-172-31-10-2.ap-northeast-2.compute.internal>
2022-10-24 15:24:28 +0000

    test_minmax (just before re-check point)

--- test/test-rbtree.c
+++ test/test-rbtree.c
@@ -71,61 +71,64 @@ void test_find_single(const key_t key, const key_t wrong_key) {
 //   delete_rbtree(t);
 // }
 
-// static void insert_arr(rbtree *t, const key_t *arr, const size_t n) {
-//   for (size_t i = 0; i < n; i++) {
-//     rbtree_insert(t, arr[i]);
-//   }
-// }
+static void insert_arr(rbtree *t, const key_t *arr, const size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    printf("i:%ld\n", i);
+    rbtree_insert(t, arr[i]);
+  }
+}
 
-// static int comp(const void *p1, const void *p2) {
-//   const key_t *e1 = (const key_t *)p1;
-//   const key_t *e2 = (const key_t *)p2;
-//   if (*e1 < *e2) {
-//     return -1;
-//   } else if (*e1 > *e2) {
-//     return 1;
-//   } else {
-//     return 0;
-//   }
-// };
+static int comp(const void *p1, const void *p2) {
+  const key_t *e1 = (const key_t *)p1;
+  const key_t *e2 = (const key_t *)p2;
+  if (*e1 < *e2) {
+    return -1;
+  } else if (*e1 > *e2) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
 
-// // min/max should return the min/max value of the tree
-// void test_minmax(key_t *arr, const size_t n) {
-//   // null array is not allowed
-//   assert(n > 0 && arr != NULL);
+// min/max should return the min/max value of the tree
+void test_minmax(key_t *arr, const size_t n) {
+  // null array is not allowed
+  assert(n > 0 && arr != NULL);
+  rbtree *t = new_rbtree();
+  assert(t != NULL);
 
-//   rbtree *t = new_rbtree();
-//   assert(t != NULL);
+  insert_arr(t, arr, n);
+  assert(t->root != NULL);
 
-//   insert_arr(t, arr, n);
-//   assert(t->root != NULL);
-// #ifdef SENTINEL
-//   assert(t->root != t->nil);
-// #endif
+#ifdef SENTINEL
+  assert(t->root != t->nil);
+#endif
 
-//   qsort((void *)arr, n, sizeof(key_t), comp);
-//   node_t *p = rbtree_min(t);
-//   assert(p != NULL);
-//   assert(p->key == arr[0]);
+  qsort((void *)arr, n, sizeof(key_t), comp);
+  node_t *p = rbtree_min(t);
+  assert(p != NULL);
+  
+  assert(p->key == arr[0]);
 
-//   node_t *q = rbtree_max(t);
-//   assert(q != NULL);
-//   assert(q->key == arr[n - 1]);
+  node_t *q = rbtree_max(t);
+  assert(q != NULL);
+  assert(q->key == arr[n - 1]);
 
-//   rbtree_erase(t, p);
-//   p = rbtree_min(t);
-//   assert(p != NULL);
-//   assert(p->key == arr[1]);
+  // re-check here!!(it means, getting min and max correctly)
+  rbtree_erase(t, p);
+  p = rbtree_min(t);
+  assert(p != NULL);
+  assert(p->key == arr[1]);
 
-//   if (n >= 2) {
-//     rbtree_erase(t, q);
-//     q = rbtree_max(t);
-//     assert(q != NULL);
-//     assert(q->key == arr[n - 2]);
-//   }
+  if (n >= 2) {
+    rbtree_erase(t, q);
+    q = rbtree_max(t);
+    assert(q != NULL);
+    assert(q->key == arr[n - 2]);
+  }
 
-//   delete_rbtree(t);
-// }
+  delete_rbtree(t);
+}
 
 // void test_to_array(rbtree *t, const key_t *arr, const size_t n) {
 //   assert(t != NULL);
@@ -293,11 +296,11 @@ void test_find_single(const key_t key, const key_t wrong_key) {
 //   test_rb_constraints(entries, n);
 // }
 
-// void test_minmax_suite() {
-//   key_t entries[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12};
-//   const size_t n = sizeof(entries) / sizeof(entries[0]);
-//   test_minmax(entries, n);
-// }
+void test_minmax_suite() {
+  key_t entries[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12};
+  const size_t n = sizeof(entries) / sizeof(entries[0]);
+  test_minmax(entries, n);
+}
 
 // void test_to_array_suite() {
 //   rbtree *t = new_rbtree();
@@ -373,7 +376,7 @@ int main(void) {
   test_find_single(512, 1024);
   // test_erase_root(128);
   // test_find_erase_fixed();
-  // test_minmax_suite();
+  test_minmax_suite();
   // test_to_array_suite();
   // test_distinct_values();
   // test_duplicate_values();
